b5321fc843cdac3298b80e88ea3f3eeec3ea43f7
fethi <fethiguneyy@gmail.com>
2024-04-24 23:25:59 +0300

    2404

---
+++ day01/js/02-script.js
@@ -0,0 +1,17 @@
+
+
+
+
+//1-console log
+console.log("Hello World");
+
+//2-console warn
+
+console.warn("Warning");
+
+//3-console error
+console.error("Error");
+
+//4-console info
+console.info("Info");
+
