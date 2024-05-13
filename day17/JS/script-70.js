f04fd9c0b218404f2abfe661446d5ad08cb2e4fb
fethi <fethiguneyy@gmail.com>
2024-05-13 23:35:44 +0300

    1305

---
+++ day17/JS/script-70.js
@@ -0,0 +1,37 @@
+import {students} from "../JS/student.js";
+
+
+document.getElementById("btnShowLowScores").addEventListener("click", ()=>{
+    setRowColorsForLowScores();
+}); 
+
+ const setRowColorsForLowScores = () => {
+
+    const tableRows = document.querySelectorAll("#tblStudents tbody tr");
+    tableRows.forEach( (tr) => {
+
+        const score = tr.querySelector("td:nth-child(3)").textContent;
+        if(score < 50){
+            tr.classList.add("table-danger");
+        }
+
+    });
+
+ }
+
+const loadData =() => {
+
+    let html ="";
+    students.forEach((student, index) => {
+        html+=`<tr>
+                    <td>${index+1}</td>
+                    <td>${student.name}</td>
+                    <td>${student.point}</td>
+                </tr>`; 
+    });
+
+    const tbody=document.querySelector("#tblStudents tbody");
+    tbody.innerHTML=html;
+}
+
+loadData();
